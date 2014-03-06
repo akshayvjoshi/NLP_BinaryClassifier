4c135e453718fa2b030a7e239aa4ac93d9f5e69c
Ravikiran Patil <patil.ravikiran@gmail.com>
2014-03-06 15:58:32 -0500

    Added test py script to experiment with calculated language model

--- get_unigram.py
+++ get_unigram.py
@@ -7,18 +7,24 @@ from pprint import pprint
 
 
 
-def get_unigram(Classlabel,path):
+def get_unigram(Classlabel,path,IterSet):
 	"Function to get the unigram model of the given Classlabel(pos/neg) using files in the path"
-	filepath = path+"/"+Classlabel
-	files = os.listdir(filepath);
+	#filepath = path
+	#files = os.listdir(filepath);
 	Prob_dict = dict();
-	for fil in files:
-		fh=open(filepath+"/"+fil,"r")
-		lines = fh.read()
-		lines = re.sub("[()+.,\']",'',lines)
-		words = nltk.tokenize.word_tokenize(lines)
-		freq_dist_unigram = FreqDist(words)
-		Prob_dict.update(freq_dist_unigram)
+	
+	for idx in IterSet:
+		fpath = path + "/" + str(idx) + "/" + Classlabel
+		print fpath
+		files = os.listdir(fpath)
+		for fil in files:
+			fh=open(fpath+"/"+fil,"r")
+			lines = fh.read()
+			lines = re.sub("[()+.,\']",'',lines)
+			words = nltk.tokenize.word_tokenize(lines)
+			freq_dist_unigram = FreqDist(words)
+			Prob_dict.update(freq_dist_unigram)
+	
 	prob_sum = 0
 	num_words = 0
 	for elem in Prob_dict.values():
@@ -26,15 +32,15 @@ def get_unigram(Classlabel,path):
 
 	for k in Prob_dict:
 		val = Prob_dict[k] 
-		Prob_dict[k] = float(val)/float(num_words)
-		prob_sum = prob_sum + Prob_dict[k]
+		Prob_dict[k] = val#/float(num_words)
+		#prob_sum = prob_sum + Prob_dict[k]
 	#print Prob_dict	
-	print prob_sum
+	#print prob_sum
 	return Prob_dict
 	
 
 if __name__ == "__main__":
-    #temp = get_unigram('pos','/home/chintu/NLP/hw1/dataset/1')
-    temp = get_unigram('neg','/home/chintu/NLP/hw1/dataset/1')
+    #temp = get_unigram('pos','/home/chintu/NLP/hw1/dataset',set([1,2,3,4]))
+    temp = get_unigram('neg','/home/chintu/NLP/hw1/dataset',set([1,2,3,4]))
     #print temp
 
--- test.py
+++ test.py
@@ -1,8 +1,74 @@
 from sets import Set
+from get_unigram import get_unigram
+import nltk
+import re
+import os
+from math import *
+
+def perplexity(Prob_dist, filename):
+	fh = open(filename, 'r')
+        lines = fh.read()
+        lines = re.sub("[()+.,\']",'',lines)
+        words = nltk.tokenize.word_tokenize(lines)
+	N = 0
+	for v in Prob_dist.values():
+		N=N+v
+	prob = float(0)
+	v= len(words)
+	for word in words:
+		if word in Prob_dist.keys():
+			prob = prob + log10(float(Prob_dist[word])/float(N))
+		else:
+				
+			prob = prob + 0.0
+
+	perplex = float(-1)*(float(1)/float(v))*prob
+	perplex = 10**perplex
+	print perplex
+	return perplex
 
 myset = Set([1,2,3,4,5]);
+temp = myset
+
+Pos_Dict= dict()
+Neg_Dict= dict()
 for x in myset:
-	for y in myset:
-		if y != x:
-			# y is the index for training set, x is the test set
-			print y
+	temp.remove(x)
+        Pos_Dict = get_unigram('pos','/home/chintu/NLP/hw1/dataset',temp)
+	# Calculate pos_perplexity
+	print len(Pos_Dict)
+        Neg_Dict = get_unigram('neg','/home/chintu/NLP/hw1/dataset',temp)
+	print len(Neg_Dict)
+	# Calculate neg_perplexity
+	fpath = '/home/chintu/NLP/hw1/dataset/'+str(x)
+	test_file_p = os.listdir(fpath +'/pos')
+	test_file_n = os.listdir(fpath+'/neg') 
+	print "Test folder:",x
+	#test positive folder under test folder
+	
+	for test in test_file_p:
+		pos_perp = perplexity(Pos_Dict, fpath+"/pos/"+test)
+		neg_perp = perplexity(Neg_Dict, fpath+"/pos/"+test)
+		if pos_perp < neg_perp:
+			print "original pos\t"+test+"\tpred pos"
+		else:
+			print "original pos\t"+test+"\tpred neg"
+	#test negative folder under test folder
+	'''
+	for test in test_file_n:
+		
+                pos_perp = perplexity(Pos_Dict, fpath+"/neg/"+test) 
+                print pos_perp
+	
+		neg_perp = perplexity(Neg_Dict, fpath+"/neg/"+test) 
+        	print neg_perp
+	        if pos_perp < neg_perp:
+                        print "original neg\t"+test+"\tpred pos"
+                else:
+                        print "original neg\t"+test+"\tpred neg"
+		 
+ 	'''
+	temp.add(x)
+	break
+	
+#print Pos_Dict
